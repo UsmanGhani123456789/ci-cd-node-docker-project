50101e3177d83d59c5c3caffcd262ff8150f9c6a
Ghani <usman.siddiq200@gmail.com>
2026-01-25 07:16:25 +0500

    Update app.js to run HTTP server

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello, welcome to the CI/CD demo application!");
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, welcome to the CI/CD demo application!');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
